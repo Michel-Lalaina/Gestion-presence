2be8bf5219d201a8d545c58849d6c3f741dd82b2
Michel-Lalaina <michelramanantenasoa@gmail.com>
2025-11-12 11:40:07 +0200

    architecture

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,12 @@
+import ArchitectureTree from "./components/ArchitectureTree";
 
 function App() {
 
   return (
 
-    <div className="text-gray-800"> Eto ny point d ' entrer App atsika </div>
+    <div className="p-6"> Eto ny point d ' entrer App <h1 className="text-green-700">MambaTECH</h1> 
+    <ArchitectureTree/>
+    </div>
 
   );
 }
