758227bb66e08cd67d783b2844fed7c04ee2f668
Michel-Lalaina <michelramanantenasoa@gmail.com>
2025-12-06 12:29:32 +0300

    mis en place API CR  presence

--- src/page/PresenceList.tsx
+++ src/page/PresenceList.tsx
@@ -1,22 +1,81 @@
-import {
-  Button,
-  TextField,
-  MenuItem,
-  Avatar,
-  Pagination,
-} from "@mui/material";
+import {Button, TextField, MenuItem, Avatar, Pagination,} from "@mui/material";
 import SearchIcon from "@mui/icons-material/Search";
 import PictureAsPdfIcon from "@mui/icons-material/PictureAsPdf";
 import GridOnIcon from "@mui/icons-material/GridOn";
-
 import { useEffect, useState } from "react";
-import { fetchPresence, Presence } from "../services/PresenceService";
+
 const statusColors: Record<string, string> = {
   Présent: "bg-green-600 text-white",
   Absent: "bg-red-500 text-white",
   Retard: "bg-yellow-500 text-white",
 };
 
+ interface Presence {
+  id: number;
+  name: string;
+  avatar: string;
+  cours: string;
+  entree: string;
+  sortie: string;
+  statut: string;
+}
+
+const presenceData: Presence[] = [
+  {
+    id: 1,
+    name: "Léa Dubois",
+    avatar: "https://i.pravatar.cc/150?img=32",
+    cours: "Développement Web",
+    entree: "09:02",
+    sortie: "11:58",
+    statut: "Présent",
+  },
+  {
+    id: 2,
+    name: "Marc Petit",
+    avatar: "https://i.pravatar.cc/150?img=15",
+    cours: "Bases de Données",
+    entree: "-",
+    sortie: "-",
+    statut: "Absent",
+  },
+  {
+    id: 3,
+    name: "Chloé Martin",
+    avatar: "https://i.pravatar.cc/150?img=45",
+    cours: "Développement Web",
+    entree: "09:12",
+    sortie: "12:01",
+    statut: "Retard",
+  },
+  {
+    id: 4,
+    name: "Hugo Bernard",
+    avatar: "https://i.pravatar.cc/150?img=27",
+    cours: "Réseaux Informatiques",
+    entree: "14:00",
+    sortie: "16:45",
+    statut: "Présent",
+  },
+  {
+    id: 5,
+    name: "Alice Fournier",
+    avatar: "https://i.pravatar.cc/150?img=18",
+    cours: "Bases de Données",
+    entree: "09:05",
+    sortie: "11:55",
+    statut: "Présent",
+  },
+];
+
+// Simule un appel API GET /presence
+export function fetchPresence(): Promise<Presence[]> {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve(presenceData);
+    }, 800); // 800ms comme un vrai appel réseau
+  });
+}
 
 export default function PresenceList() {
   const [presenceData, setPresenceData] = useState<Presence[]>([]);
--- src/services/PresenceService.tsx
+++ src/services/PresenceService.tsx
@@ -1,75 +1,5 @@
 // src/services/presenceService.ts
 import axios from "axios";
-
-export interface Presence {
-  id: number;
-  name: string;
-  avatar: string;
-  cours: string;
-  entree: string;
-  sortie: string;
-  statut: string;
-}
-
-const presenceData: Presence[] = [
-  {
-    id: 1,
-    name: "Léa Dubois",
-    avatar: "https://i.pravatar.cc/150?img=32",
-    cours: "Développement Web",
-    entree: "09:02",
-    sortie: "11:58",
-    statut: "Présent",
-  },
-  {
-    id: 2,
-    name: "Marc Petit",
-    avatar: "https://i.pravatar.cc/150?img=15",
-    cours: "Bases de Données",
-    entree: "-",
-    sortie: "-",
-    statut: "Absent",
-  },
-  {
-    id: 3,
-    name: "Chloé Martin",
-    avatar: "https://i.pravatar.cc/150?img=45",
-    cours: "Développement Web",
-    entree: "09:12",
-    sortie: "12:01",
-    statut: "Retard",
-  },
-  {
-    id: 4,
-    name: "Hugo Bernard",
-    avatar: "https://i.pravatar.cc/150?img=27",
-    cours: "Réseaux Informatiques",
-    entree: "14:00",
-    sortie: "16:45",
-    statut: "Présent",
-  },
-  {
-    id: 5,
-    name: "Alice Fournier",
-    avatar: "https://i.pravatar.cc/150?img=18",
-    cours: "Bases de Données",
-    entree: "09:05",
-    sortie: "11:55",
-    statut: "Présent",
-  },
-];
-
-// Simule un appel API GET /presence
-export function fetchPresence(): Promise<Presence[]> {
-  return new Promise((resolve) => {
-    setTimeout(() => {
-      resolve(presenceData);
-    }, 800); // 800ms comme un vrai appel réseau
-  });
-}
-
-
-
 const API_URL = "http://127.0.0.1:8000";
 
 
@@ -79,6 +9,16 @@ export const getListPresence = async () => {
     return response.data; // On suppose que le backend renvoie { question, options, quizId }
   } catch (error) {
     console.error("Erreur lors de la récupération des eleves", error);
+    throw error;
+  }
+};
+
+export const createEleve = async (name: string, cours: string, statut: string) => {
+  try {
+    const response = await axios.post(`${API_URL}/elevCreat`, { name, cours, statut });
+    return response.data;
+  } catch (error) {
+    console.error("Erreur lors de l'ajout du quiz", error);
     throw error;
   }
 };
