14c748361cec8041c46ebd6ca3c454bf5feff1e4
Michel-Lalaina <michelramanantenasoa@gmail.com>
2025-12-08 14:49:54 +0200

    mis a jour

--- src/services/PresenceList.tsx
+++ src/services/PresenceList.tsx
@@ -1,12 +1,12 @@
 // src/services/presenceService.ts
 import axios from "axios";
-const API_URL = "http://127.0.0.1:8000";
+const API_URL = "http://127.0.0.1:8000"; //URL not true
 
 
 export const getListPresence = async () => {
   try {
-    const response = await axios.get(`${API_URL}/presenceLists`); // Remplacez par l'URL de votre backend
-    return response.data; // On suppose que le backend renvoie { question, options, quizId }
+    const response = await axios.get(`${API_URL}/presenceLists`);
+    return response.data;
   } catch (error) {
     console.error("Erreur lors de la récupération du list", error);
     throw error;
